1dddbe40cdb57670b82d2ee7b33ee0a240fdb891
Julius Padi <padijnmm@gmail.com>
2023-02-17 18:44:24 +0000

    Update

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+mongoose.set('strictQuery', false);
+
 const app = express();
 const port = process.env.PORT || 3000;
 
